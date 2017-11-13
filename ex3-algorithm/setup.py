a9fc4cf8132e667f9a49b0f2b1458c7f67179837
Jerome Kelleher <jk@well.ox.ac.uk>
2017-11-13 12:35:18 +0000

    Added missing files.

---
+++ ex3-algorithm/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, Extension
+
+demo_module = Extension(
+    'demo',
+    sources=["demomodule.c", "algorithm.c"]
+)
+
+setup(
+    name="demo",
+    description="Demo C module",
+    ext_modules=[demo_module])
+
